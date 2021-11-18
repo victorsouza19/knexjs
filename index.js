828d8f5bddebdfc1d46a6ddab0a62fea914f7706
victorsouza19 <victor.cristofalo@hotmail.com>
2021-11-17 23:52:49 -0300

    new queries with 1:1 and 1:n  relationships

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const database = require('./database');
 
-database.select().table("games").orderBy("price", "asc").then(data => {
-  console.log(data);
+database.select([
+  "games.*",
+  "studios.name as studio"
+  ]).table("games").innerJoin("studios","studios.game_id", "games.id").then(data => {
+  let studiosGamesArr = data;
+
+  let game = {
+    id: 0,
+    name: '',
+    studios: []
+  };
+
+  game.id = data[0].id;
+  game.name = data[0].name;
+
+  data.forEach(result => {
+    game.studios.push({name: result.studio});
+  });
+  console.log(game);
+  
 }).catch(err => {
   console.log(err);
 });
--- queries.js
+++ queries.js
@@ -106,5 +106,79 @@ database.select().table("games").orderBy("price", "desc").then(data => {
   console.log(err);
 });
 
-/* table 1:1 */
+/* table 1:1 - Associated Insert */
+database.insert({
+  name: "Rockstar Games",
+  game_id: 2
+}).table('studios').then(data => {
+  console.log(data);
+}).catch(err => {
+  console.log(err);
+});
 
+/* table 1:1 - Associated select */
+database.select().table("games").innerJoin("studios","studios.game_id", "games.id").then(data => {
+  console.log(data);
+}).catch(err => {
+  console.log(err);
+});
+
+database.select([
+  "games.*",
+  "studios.name as studio"
+  ]).table("games").innerJoin("studios","studios.game_id", "games.id").then(data => {
+  console.log(data);
+}).catch(err => {
+  console.log(err);
+});
+
+
+/* 1:1 join with where clause */
+database.select([
+  "games.*",
+  "studios.name as studio"
+  ]).table("games")
+  .leftJoin("studios","studios.game_id", "games.id")
+  .where("games.id", 2)
+  .then(data => {
+    console.log(data);
+  }).catch(err => {
+    console.log(err);
+});
+
+
+/* 1:n Associated insert is the same scenario of the 1:1 tables */
+database.insert({
+  name: "Riot",
+  game_id: 2
+}).table('studios').then(data => {
+  console.log(data);
+}).catch(err => {
+  console.log(err);
+});
+
+
+/* 1:n Associated select is the same scenario of the 1:1 tables */
+database.select([
+  "games.*",
+  "studios.name as studio"
+  ]).table("games").innerJoin("studios","studios.game_id", "games.id").then(data => {
+
+  // studios to array example
+  let game = {
+    id: 0,
+    name: '',
+    studios: []
+  };
+
+  game.id = data[0].id;
+  game.name = data[0].name;
+
+  data.forEach(result => {
+    game.studios.push({name: result.studio});
+  });
+  console.log(game);
+  
+}).catch(err => {
+  console.log(err);
+});
